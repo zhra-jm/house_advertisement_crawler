e70e34c7168a74c2a0bc948295f9cce8585f092f
Your Name <zahrajam1999@gmail.com>
2021-02-22 23:48:42 +0330

    step5-all_done

--- parser.py
+++ parser.py
@@ -4,11 +4,10 @@ from bs4 import BeautifulSoup
 class AdvertisementPageParser:
     def parse(self, html_data):
         soup = BeautifulSoup(html_data, 'html.parser')
-        # data = dict(
-        #     title=None, price=None, body=None, post_id=None,
-        #     created_time=None, modified_time=None
-        # )
-        data = data = dict(title=None, price=None, body=None)
+        data = dict(
+            title=None, price=None, body=None, post_id=None,
+            created_time=None, modified_time=None
+        )
         title_tag = soup.find('span', attrs={'id': 'titletextonly'})
         if title_tag:
             data['title'] = title_tag.text
@@ -20,8 +19,8 @@ class AdvertisementPageParser:
         body_tag = soup.select_one('#postingbody')
         if body_tag:
             data['body'] = body_tag.text
-
-        # post_id_tag = soup.find('p', attrs={'title': 'post id'})
+        #
+        # post_id_tag = soup.find('p', attrs={'class': 'postinginfo'})
         # if post_id_tag:
         #     data['post_id'] = post_id_tag.text
         #
@@ -32,4 +31,4 @@ class AdvertisementPageParser:
         # modified_time_tag = soup.find('p', attrs={'class':"postinginfo reveal"})
         # if modified_time_tag:
         #     data['modified_time'] = modified_time_tag.text
-        return data
+        # return data
